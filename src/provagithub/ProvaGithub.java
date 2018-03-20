3a7c12ed386588df4e8cc41e696a2a4fc7440fd5
marcmestre <marcmestre@marcmestrePC>
2018-03-20 09:10:29 +0100

    he afegit un segon missatge

--- src/provagithub/ProvaGithub.java
+++ src/provagithub/ProvaGithub.java
@@ -17,6 +17,7 @@ public class ProvaGithub {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola github");
+        System.out.println("muntada github");
     }
     
 }
